bd2a8aba7f8ab265faf9b523a33d54cb654ab96a
allan@thetechtribe.com <allan@thetechtribe.com>
2024-01-23 10:20:05 +0800

    update bootstrap library to version 5.3.2

--- README.txt
+++ README.txt
@@ -4,7 +4,7 @@ Donate link: thetechtribe.com
 Tags: techtribe, content, syndication
 Requires at least: 5.0
 Tested up to: 6.4.2
-Stable tag: 1.3.0
+Stable tag: 1.3.1
 Requires PHP: 7.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -58,6 +58,10 @@ This plugin automatically pulls down those Blog Posts and publishes them on thei
 1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Screenshots are stored in the /assets directory.
 
 == Changelog ==
+
+= 1.3.1 - January 22, 2024 =
+* Update bootstrap library to version 5.3.2
+* Maintenance Update
 
 = 1.3.0 - January 08, 2024 =
 * Fix warning popup message
--- admin/class-the-tribal-plugin-admin.php
+++ admin/class-the-tribal-plugin-admin.php
@@ -75,7 +75,7 @@ class The_Tribal_Plugin_Admin {
 
 		if( tttAllowedAdminAssetInclude() ){
 			wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/the-tribal-plugin-admin.css', array(), $this->version, 'all' );
-			wp_enqueue_style( $this->plugin_name . 'bootstrap-5-iso', tttc_get_plugin_dir_url() . 'assets/css/bootstrap-iso-v5.1.3.min.css', array(), '5.1.3', 'all' );
+			wp_enqueue_style( $this->plugin_name . '-bootstrap-5-iso', tttc_get_plugin_dir_url() . 'assets/css/bootstrap-iso-v5.3.2.min.css', array(), '5.3.2', 'all' );
 		}
 
 	}
@@ -100,7 +100,7 @@ class The_Tribal_Plugin_Admin {
 		 */
 		if( tttAllowedAdminAssetInclude() ){
 			wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/the-tribal-plugin-admin.js', array( 'jquery' ), $this->version, false );
-			wp_enqueue_script( $this->plugin_name . 'bootstrap-5-iso', tttc_get_plugin_dir_url() . 'assets/js/bootstrap-v5.1.3-.bundle.min.js', array( 'jquery' ), '5.1.3', false );
+			wp_enqueue_script( $this->plugin_name . '-bootstrap-5-iso', tttc_get_plugin_dir_url() . 'assets/js/bootstrap-v5.3.2.bundle.min.js', array( 'jquery' ), '5.3.2', false );
 			wp_localize_script( $this->plugin_name, 'ttt_admin_ajax_object',
 				[
 					'plugin_url' => tttc_get_plugin_dir_url(),
